43ce1fd909fc4f30bff13f355160491a33ace3db
DizzlyD <david4henery00@hotmail.com>
2022-11-06 00:05:30 +0000

    slide-ui-respon

--- src/App.js
+++ src/App.js
@@ -16,22 +16,22 @@ function App() {
           isSlideInActive ? "slide-in slide-in-slide-active" : "slide-in"
         }
       >
-        <div className="slide-in-col-1">
-          <div className="local-info">
+        <div className="slide-in-col slide-in-col-1">
+          <div className="info-style local-info">
             <h3 className="slide-header-style">CURRENT TIMEZONE</h3>
             <h2 className="h2-header-style">Europe/London</h2>
           </div>
-          <div className="day-year-info">
+          <div className="info-style day-year-info">
             <h3 className="slide-header-style">Day of the year</h3>
             <h2 className="h2-header-style">295</h2>
           </div>
         </div>
-        <div className="slide-in-col-2">
-          <div className="day-week-info">
+        <div className="slide-in-col slide-in-col-2">
+          <div className="info-style day-week-info">
             <h3 className="slide-header-style">Day of the week</h3>
             <h2 className="h2-header-style">5</h2>
           </div>
-          <div className="week-num-info">
+          <div className="info-style week-num-info">
             <h3 className="slide-header-style">Week number</h3>
             <h2 className="h2-header-style">42</h2>
           </div>
